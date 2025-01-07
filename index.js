ce7c3300bc0885e1abd2b539d67d5235e61279e1
ArtRosh <artem.rosh17@gmail.com>
2025-01-07 15:00:31 -0800

    arithmetic-lab

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js (or root.js in your case)
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return 'Cannot divide by zero';
+    }
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
+  module.exports = { add, subtract, multiply, divide, increment, decrement, makeInt, preserveDecimal };
